d38dcc0a087d855c0898b49195821b072d7b70ee
Dimitrios <tsiridakis@yahoo.gr>
2018-03-26 21:23:59 +0300

    Test POST/Todos Route

--- server/server.js
+++ server/server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+
+
 const mongoose = require('./db/mongoose');
 const Todo = require('./models/todo');
 const User = require('./models/user');
 
-var app = express();
+const app = express();
 app.use(bodyParser.json());
 
 
-
 app.post('/todos', (req, res) => {
-  var postmanTodo = new Todo({
+  let postmanTodo = new Todo({
     text: req.body.text
   });
 
@@ -26,3 +27,5 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = app;
